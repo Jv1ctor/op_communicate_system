5da83d4edf9c2afcb643ad50431ed299f4904eb7
João Victor <j.rodriguesbarretos@gmail.com>
2023-10-30 16:52:39 -0300

    produto page js expansive menu and style page

--- public/js/produto.js
+++ public/js/produto.js
@@ -1,5 +1,6 @@
 const reactorName = document.querySelector('[data-js="reactor-name"]')
-
+const tableContainer = document.querySelector("[data-js='tables']")
+const tableList = document.querySelectorAll(".main-produto-table")
 const renderReactorName = () => {
   const query = window.location.search
   const params = new URLSearchParams(query)
@@ -8,4 +9,67 @@ const renderReactorName = () => {
   reactorName.textContent = `Reator ${name}`
 }
 
+const hiddenTables = () => {}
+
 renderReactorName()
+
+tableContainer.addEventListener("click", (e) => {
+  tableList.forEach((divTable) => {
+    if (e.target.dataset.js === "show-andamento") {
+      const table = divTable.querySelector("table")
+      const arrow = divTable.querySelector(".arrow")
+      if (divTable.classList.contains("andamento")) {
+        let arrow = ""
+        if (e.target.classList.contains("arrow")) {
+          arrow = e.target
+        } else {
+          arrow = e.target.children[1]
+        }
+
+        arrow.classList.toggle("active-arrow")
+        table.classList.toggle("active-table")
+      } else {
+        table.classList.remove("active-table")
+        arrow.classList.remove("active-arrow")
+      }
+    }
+
+    if (e.target.dataset.js === "show-aprovados") {
+      const table = divTable.querySelector("table")
+      const arrow = divTable.querySelector(".arrow")
+      if (divTable.classList.contains("aprovados")) {
+        let arrow = ""
+        if (e.target.classList.contains("arrow")) {
+          arrow = e.target
+        } else {
+          arrow = e.target.children[1]
+        }
+
+        arrow.classList.toggle("active-arrow")
+        table.classList.toggle("active-table")
+      } else {
+        table.classList.remove("active-table")
+        arrow.classList.remove("active-arrow")
+      }
+    }
+
+    if (e.target.dataset.js === "show-reprovados") {
+      const table = divTable.querySelector("table")
+      const arrow = divTable.querySelector(".arrow")
+      if (divTable.classList.contains("reprovados")) {
+        let arrow = ""
+        if (e.target.classList.contains("arrow")) {
+          arrow = e.target
+        } else {
+          arrow = e.target.children[1]
+        }
+
+        arrow.classList.toggle("active-arrow")
+        table.classList.toggle("active-table")
+      } else {
+        table.classList.remove("active-table")
+        arrow.classList.remove("active-arrow")
+      }
+    }
+  })
+})
