b714ad3dbb74b00eb3d607e6e45f8fb9e612b718
Jv1ctor <j.rodriguesbarretos@gmail.com>
2023-10-31 21:08:59 -0300

    refactory produto.js

--- public/js/produto.js
+++ public/js/produto.js
@@ -1,6 +1,7 @@
 const reactorName = document.querySelector('[data-js="reactor-name"]')
 const tableContainer = document.querySelector("[data-js='tables']")
 const tableList = document.querySelectorAll(".main-produto-table")
+
 const renderReactorName = () => {
   const query = window.location.search
   const params = new URLSearchParams(query)
@@ -9,67 +10,42 @@ const renderReactorName = () => {
   reactorName.textContent = `Reator ${name}`
 }
 
-const hiddenTables = () => {}
+const currentArrow = (elemento) => {
+  const existArrow = elemento.classList.contains("arrow")
+  let arrowElement = ""
+  if (existArrow) {
+    return (arrowElement = elemento)
+  } else {
+    return (arrowElement = elemento.children[1])
+  }
+}
 
-renderReactorName()
+const menuEvent = (context, divTable, elemento) => {
+  const table = divTable.querySelector("table")
+  const isShowMenu = divTable.classList.contains(context)
+  const arrowElement = currentArrow(elemento)
+  if (isShowMenu) {
+    arrowElement.classList.toggle("active-arrow")
+    table.classList.toggle("active-table")
+  } else {
+    table.classList.remove("active-table")
+    arrowElement.classList.remove("active-arrow")
+  }
+}
 
-tableContainer.addEventListener("click", (e) => {
+const showTablesMenu = (e) => {
   tableList.forEach((divTable) => {
     if (e.target.dataset.js === "show-andamento") {
-      const table = divTable.querySelector("table")
-      const arrow = divTable.querySelector(".arrow")
-      if (divTable.classList.contains("andamento")) {
-        let arrow = ""
-        if (e.target.classList.contains("arrow")) {
-          arrow = e.target
-        } else {
-          arrow = e.target.children[1]
-        }
-
-        arrow.classList.toggle("active-arrow")
-        table.classList.toggle("active-table")
-      } else {
-        table.classList.remove("active-table")
-        arrow.classList.remove("active-arrow")
-      }
+      menuEvent("andamento", divTable, e.target)
     }
-
     if (e.target.dataset.js === "show-aprovados") {
-      const table = divTable.querySelector("table")
-      const arrow = divTable.querySelector(".arrow")
-      if (divTable.classList.contains("aprovados")) {
-        let arrow = ""
-        if (e.target.classList.contains("arrow")) {
-          arrow = e.target
-        } else {
-          arrow = e.target.children[1]
-        }
-
-        arrow.classList.toggle("active-arrow")
-        table.classList.toggle("active-table")
-      } else {
-        table.classList.remove("active-table")
-        arrow.classList.remove("active-arrow")
-      }
+      menuEvent("aprovados", divTable, e.target)
     }
-
     if (e.target.dataset.js === "show-reprovados") {
-      const table = divTable.querySelector("table")
-      const arrow = divTable.querySelector(".arrow")
-      if (divTable.classList.contains("reprovados")) {
-        let arrow = ""
-        if (e.target.classList.contains("arrow")) {
-          arrow = e.target
-        } else {
-          arrow = e.target.children[1]
-        }
-
-        arrow.classList.toggle("active-arrow")
-        table.classList.toggle("active-table")
-      } else {
-        table.classList.remove("active-table")
-        arrow.classList.remove("active-arrow")
-      }
+      menuEvent("reprovados", divTable, e.target)
     }
   })
-})
+}
+
+renderReactorName()
+tableContainer.addEventListener("click", showTablesMenu)
