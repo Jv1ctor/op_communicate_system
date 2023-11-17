584a291a9084cac33f503cd282f1f3b21b26d05d
João Victor <j.rodriguesbarretos@gmail.com>
2023-11-17 14:34:59 -0300

    insert icon from fontawesome

--- public/js/analise.js
+++ public/js/analise.js
@@ -1,33 +1,33 @@
-// const productName = document.querySelector("[data-js='product-name']")
+const productName = document.querySelector("[data-js='product-name']")
 
-// const modal = document.querySelector("[data-js='modal']")
-// const buttonAddProduct = document.querySelector("[data-js='add-analise']")
-// const modalForm = document.querySelector("[data-js='form-register-analise']")
-// const renderProductName = () => {
-//   const query = window.location.search
-//   const params = new URLSearchParams(query)
-//   const name = params.get("analise")
-//   productName.textContent = `${name}`
-// }
+const modal = document.querySelector("[data-js='modal']")
+const buttonAddProduct = document.querySelector("[data-js='add-analise']")
+const modalForm = document.querySelector("[data-js='form-register-analise']")
+const renderProductName = () => {
+  const query = window.location.search
+  const params = new URLSearchParams(query)
+  const name = params.get("analise")
+  productName.textContent = `${name}`
+}
 
-// const openModalProduct = (e) => {
-//   modal.classList.toggle("show-modal")
-//   document.body.classList.toggle("show-modal")
-// }
+const openModalProduct = (e) => {
+  modal.classList.toggle("show-modal")
+  document.body.classList.toggle("show-modal")
+}
 
-// const closeModalProduct = (e) => {
-//   if (e.target.dataset.js === "button-exit") {
-//     modal.classList.toggle("show-modal")
-//     document.body.classList.toggle("show-modal")
-//   }
+const closeModalProduct = (e) => {
+  if (e.target.dataset.js === "button-exit") {
+    modal.classList.toggle("show-modal")
+    document.body.classList.toggle("show-modal")
+  }
 
-//   if (e.target.dataset.js === "modal") {
-//     modal.classList.toggle("show-modal")
-//     document.body.classList.toggle("show-modal")
-//   }
-// }
+  if (e.target.dataset.js === "modal") {
+    modal.classList.toggle("show-modal")
+    document.body.classList.toggle("show-modal")
+  }
+}
 
-// renderProductName()
-// // modalForm.addEventListener("input", formattingInputs)
-// buttonAddProduct.addEventListener("click", openModalProduct)
-// modal.addEventListener("click", closeModalProduct)
+renderProductName()
+// modalForm.addEventListener("input", formattingInputs)
+buttonAddProduct.addEventListener("click", openModalProduct)
+modal.addEventListener("click", closeModalProduct)
